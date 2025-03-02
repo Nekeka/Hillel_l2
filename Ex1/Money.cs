ebbed4b62103c7cd998227459918fc0523c5a742
Mykyta Khomik <n.homick4@gmail.com>
2025-03-02 18:38:31 +0200

    Add Money base class

---
+++ Ex1/Money.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Money
+    {
+
+        public int Dollars
+        {
+            get;
+            private set;
+        }
+
+        public int Cents
+        {
+            get;
+            private set;
+        }
+
+        public void PrintMoney()
+        {
+            Console.WriteLine($"Dollars: {Dollars}\nCents: {Cents}");
+        }
+
+        public void SetMoney(int dollars, int cents)
+        {
+            if (dollars >= 0)
+                Dollars = dollars;
+            else
+                throw new Exception("dollars must be >=0");
+               
+            if (cents>=0 && cents<=99)
+                Cents = cents;
+            else
+                throw new Exception("cents must be >=0 and <100");
+        }
+       public Money()
+        {
+            Dollars = 0;
+            Cents = 0;
+        }
+        public Money(int money)
+        {
+            Dollars = money;
+            Cents = 0;
+        }
+        public Money(int dollars, int cents)
+        {
+            Dollars = dollars;
+            Cents = cents;
+        }
+
+    }
+}
---
+++ Ex1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Product : Money
+    {
+        public string ProductName { get; private set; }
+
+        public Product(string name): base()
+        {
+            ProductName = name;
+        }
+
+        public Product(string name, int dollars) : base(dollars)
+        {
+            ProductName = name;
+        }
+        public Product(string name, int dollars, int cents) : base(dollars, cents)
+        {
+            ProductName = name;
+        }
+
+        public void ReducePrice()
+        {
+
+        }
+
+    }
+}
